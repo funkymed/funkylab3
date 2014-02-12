3c38cf4e4698c90403610f255d0d40eaa57484e4
Cyril Pereira <cyril.pereira@gmail.com>
2014-02-12 01:19:06 +0100

    correct bug

--- web/admin/includes/php/bddconf.php
+++ web/admin/includes/php/bddconf.php
@@ -4,16 +4,16 @@ switch($strServeur){
     case "localhost":
         $host       = "localhost";
         $user       = "root";
-        $pass       = "Zwc0f75h";
+        $pass       = "xxxx";
         $bdd        = "f3";
         $ext_site   = "f3";
         $url        = "http://localhost/f3.funkylab.info/web/";
         break;
     default:
         $host       = 'localhost:/tmp/mysql5.sock';
-        $user       = "dbo339428562";
-        $pass       = "vc121wt78v";
-        $bdd        = "db339428562";
+        $user       = "user";
+        $pass       = "pass";
+        $bdd        = "bdd";
         $ext_site   = "funkylab3";
         $url        = "http://localhost/f3.funkylab.info/web/";
 }
